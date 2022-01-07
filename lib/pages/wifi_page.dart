39117ca78ba1ea330e2f020a200e7aec02cfda81
Ucer <FayozbekJumabekov>
2022-01-07 14:34:20 +0500

    Wi-Fi and Switch widgets

--- lib/pages/wifi_page.dart
+++ lib/pages/wifi_page.dart
@@ -33,7 +33,7 @@ class _WiFiPageState extends State<WiFiPage> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text("Switch WiFi"),
+        title: Text("Switch Wi-Fi"),
         actions: [
           Switch(
             onChanged: (value) {
@@ -62,61 +62,64 @@ class _WiFiPageState extends State<WiFiPage> {
       ),
       body: indicator
           ? Container(
-              padding: EdgeInsets.only(top: 20, left: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Available networks : ",
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  Container(
-                      margin: EdgeInsets.only(right: 30),
-                      width: 20,
-                      height: 20,
-                      child: CircularProgressIndicator(
-                        backgroundColor:
-                            (indicator) ? Colors.white : Colors.black,
-                        color: Colors.black,
-                      )),
-                ],
-              ),
-            )
+        padding: EdgeInsets.only(top: 20, left: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Available networks : ",
+              style: TextStyle(fontSize: 20),
+            ),
+            Container(
+                margin: EdgeInsets.only(right: 30),
+                width: 20,
+                height: 20,
+                child: CircularProgressIndicator(
+                  backgroundColor:
+                  (indicator) ? Colors.white : Colors.black,
+                  color: Colors.black,
+                )),
+          ],
+        ),
+      )
           : Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 20, left: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Container(
-                  padding: EdgeInsets.only(top: 20, left: 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "Available networks : ",
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      Container(
-                          margin: EdgeInsets.only(right: 30),
-                          width: 20,
-                          height: 20,
-                          child: CircularProgressIndicator(
-                            backgroundColor: Colors.black,
-                            color: Colors.black,
-                          )),
-                    ],
-                  ),
+                Text(
+                  "Available networks : ",
+                  style: TextStyle(fontSize: 20),
                 ),
                 Container(
-                  margin: EdgeInsets.only(top: 70),
-                  width: MediaQuery.of(context).size.width,
-                  child: (isSwitched)
-                      ? ListView.builder(
-                          itemCount: wifiList.length,
-                          itemBuilder: (context, index) {
-                            return card(index);
-                          })
-                      : SizedBox.shrink(),
-                ),
+                    margin: EdgeInsets.only(right: 30),
+                    width: 20,
+                    height: 20,
+                    child: CircularProgressIndicator(
+                      backgroundColor: Colors.black,
+                      color: Colors.black,
+                    )),
               ],
             ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 70),
+            width: MediaQuery
+                .of(context)
+                .size
+                .width,
+            child: (isSwitched)
+                ? ListView.builder(
+                itemCount: wifiList.length,
+                itemBuilder: (context, index) {
+                  return card(index);
+                })
+                : SizedBox.shrink(),
+          ),
+        ],
+      ),
     );
   }
 
